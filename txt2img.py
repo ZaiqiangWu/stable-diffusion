92b1fa8ddf38b76cdab3ea042e9d431abf7b13bd
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-11-08 20:51:36 +0900

    updated

--- txt2img.py
+++ txt2img.py
@@ -1,7 +1,7 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-model_id = "senhentai_v12"
+model_id = "./"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
