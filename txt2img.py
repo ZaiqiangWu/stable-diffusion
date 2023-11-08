fe1065367285afddea2b57de3be200916a7b5f2d
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-11-08 22:07:28 +0900

    updated

--- txt2img.py
+++ txt2img.py
@@ -3,7 +3,8 @@ import torch
 from diffusers import DiffusionPipeline
 
 
-pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+#pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipe = DiffusionPipeline.from_pretrained("./ckpts/senhentai_v12.safetensors")
 
 
 def null_safety(images, **kwargs):
