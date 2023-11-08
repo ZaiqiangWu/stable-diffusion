761ebcf300d47757e728247e33448d5c9f45f35e
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-11-08 23:51:17 +0900

    updated

--- txt2img.py
+++ txt2img.py
@@ -145,7 +145,7 @@ use_prompt_embeddings = True
 
 # Seed and batch size.
 start_idx = 0
-batch_size = 10
+batch_size = 4
 seeds = [i for i in range(start_idx , start_idx + batch_size, 1)]
 
 # Number of inference steps.
@@ -196,5 +196,5 @@ def save_images(images, labels = None):
     n_rows = int(np.ceil(N / n_cols))
 
     for i in range(len(images)):
-        cv2.imwrite(str(i).zfill(2)+'.jpg',np.array(images[i]))
+        cv2.imwrite(str(i).zfill(2)+'.jpg',np.array(images[i])[:,:,[2,1,0]])
 save_images(images, seeds[:len(images)])
