64c7fb8db0517ac334eb4021d70cc5c4aa88cc39
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-11-08 21:27:22 +0900

    updated

--- txt2img.py
+++ txt2img.py
@@ -6,7 +6,7 @@ pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
 
 pipe = pipe.to("cuda")
 
-prompt = "a photo of an astronaut riding a horse on mars"
+prompt = "a photo of an topless girl"
 image = pipe(prompt).images[0]
 
 image.save("astronaut_rides_horse.png")
