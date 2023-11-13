f701680443dc2e991b2dd937d3c46afdef476e89
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-11-13 15:16:28 +0900

    updated

--- inpaint_test.py
+++ inpaint_test.py
@@ -22,7 +22,7 @@ mask_image = download_image(mask_url).resize((512, 512))
 pipe = StableDiffusionInpaintPipeline.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
 )
-pipe = pipe.to("cuda")
+#pipe = pipe.to("cuda")
 
 prompt = "Face of a yellow cat, high resolution, sitting on a park bench"
 image = pipe(prompt=prompt, image=init_image, mask_image=mask_image).images[0]
